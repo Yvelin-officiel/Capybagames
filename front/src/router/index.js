f8f5dd61f503b0236a059976abcb881fc796a7ca
Yvelin-officiel <gerbeaudy@yahoo.fr>
2026-04-29 16:13:45 +0200

    fix

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from "vue-router";
+// import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Charte from "../pages/Charte.vue";
 import Accueil from "../pages/Accueil.vue";
 import JetpackCapy from "../pages/JetpackCapy.vue";
@@ -34,7 +35,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 };
